94dac058e5aaf5c1e1f8e383e8cc727e43d6bf6c
Guillaume <breyerguillaume@gmail.com>
2023-02-09 19:33:54 +0100

    end 9-2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,42 @@
-import React from "react";
+import { React, useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  return (
-    <Card className="expenses">
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const selectFilteredYear = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((el) => {
+    return el.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((element) => (
       <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+        key={element.id}
+        title={element.title}
+        amount={element.amount}
+        date={element.date}
       />
-    </Card>
+    ));
+  }
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          OnSelectYear={selectFilteredYear}
+        />
+        {expensesContent}
+      </Card>
+    </div>
   );
 };
 
