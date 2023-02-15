e18fcc202159f9d87c8c2e9694b75f711eae9a42
Guillaume <breyerguillaume@gmail.com>
2023-02-15 19:30:18 +0100

    update portal

--- 01-starting-finalProject/src/App.js
+++ 01-starting-finalProject/src/App.js
@@ -12,10 +12,10 @@ function App() {
   };
 
   return (
-    <div>
+    <>
       <AddUser onAddUser={addUserHandler} />
       <UserList data={users} />
-    </div>
+    </>
   );
 }
 
--- 01-starting-finalProject/src/components/UI/ErrorModal.js
+++ 01-starting-finalProject/src/components/UI/ErrorModal.js
@@ -1,22 +1,45 @@
 import Card from "./Card";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
+import ReactDom from "react-dom";
+import React from "react";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onClick} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.btn}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const Modal = (props) => {
   return (
-    <div className={styles.backdrop} onClick={props.btn}>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.btn}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onClick={props.btn} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          btn={props.btn}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
